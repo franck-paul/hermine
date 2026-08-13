928f87b7fc9a95af8663dd5e64b90659967f0eae
franck <contact@open-time.net>
2026-08-13 16:49:03 +0200

    Enforces some verification before registering module

--- _define.php
+++ _define.php
@@ -9,23 +9,27 @@
  * @copyright Franck Paul (contact@open-time.net)
  * @copyright GPL-2.0
  */
-$this->registerModule(
-    'Hermine',
-    'Hermine photoblog theme (based on Berlin)',
-    'Franck Paul',
-    '8.2',
-    [
-        'date'                 => '2026-04-15T17:44:38+0200',
-        'requires'             => [['core', '2.36']],
-        'type'                 => 'theme',
-        'tplset'               => 'dotty',
-        'widgettitleformat'    => '<h3 class="widget-title">%s</h3>',
-        'widgetsubtitleformat' => '<h4 class="widget-subtitle">%s</h4>',
-        'overload'             => true,
+declare(strict_types=1);
 
-        'details'    => 'https://open-time.net/?q=hermine',
-        'support'    => 'https://github.com/franck-paul/hermine',
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/hermine/main/dcstore.xml',
-        'license'    => 'gpl2',
-    ]
-);
+if (isset($this) && is_object($this) && method_exists($this, 'registerModule') && isset($this->id) && is_string($this->id)) {
+    $this->registerModule(
+        'Hermine',
+        'Hermine photoblog theme (based on Berlin)',
+        'Franck Paul',
+        '8.2',
+        [
+            'date'                 => '2026-04-15T17:44:38+0200',
+            'requires'             => [['core', '2.36']],
+            'type'                 => 'theme',
+            'tplset'               => 'dotty',
+            'widgettitleformat'    => '<h3 class="widget-title">%s</h3>',
+            'widgetsubtitleformat' => '<h4 class="widget-subtitle">%s</h4>',
+            'overload'             => true,
+
+            'details'    => 'https://open-time.net/?q=hermine',
+            'support'    => 'https://github.com/franck-paul/hermine',
+            'repository' => 'https://raw.githubusercontent.com/franck-paul/hermine/main/dcstore.xml',
+            'license'    => 'gpl2',
+        ]
+    );
+}
